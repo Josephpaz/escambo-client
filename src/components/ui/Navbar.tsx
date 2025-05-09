d9c4bda0ce17b75280822da22f7d94b952b700f6
jspaz <joseph@suamei.com.br>
2025-05-09 10:43:45 -0400

    feat: revamp layout, navbar && nested pages

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,59 +1,59 @@
+import LogoPng from "@/assets/Logo.png";
+import {Box, Flex, Image, Link, LinkProps, Stack, Text} from "@chakra-ui/react";
+import {Avatar} from "./Avatar";
+import {CustomBreadcrumb} from "./CustomBreadcrumb";
 
-import LogoPng from '@/assets/Logo.png';
-import {
-    Box,
-    Flex,
-    HStack,
-    Image,
-    Link,
-    Spacer,
-    Stack,
-    Text
-} from "@chakra-ui/react";
-import { Avatar } from './Avatar';
-import { CustomBreadcrumb } from './CustomBreadcrumb';
+type RenderLinkProps = LinkProps;
+export function RenderLink(props: RenderLinkProps) {
+  const {children} = props;
+  return (
+    <Link
+      _active={{color: "#A0EACB", transform: "scale(0.95)"}}
+      _hover={{color: "#CFFCEE", transform: "scale(1.05)"}}
+      transition="all 0.2s ease"
+      {...props}
+    >
+      {children}
+    </Link>
+  );
+}
+
+const childrenLinks: LinkProps["children"][] = [
+  <Image
+    src={LogoPng}
+    h={53}
+    w={284}
+    mr="-3rem"
+    cursor={"pointer"}
+    textDecoration={"none"}
+  />,
+  <Text color="white" fontWeight="normal" fontSize={16} cursor="pointer">
+    Histórico
+  </Text>,
+  <Text color="white" fontWeight="normal" fontSize={16} cursor="pointer">
+    Incluir Item
+  </Text>,
+  <Text color="white" fontWeight="normal" fontSize={16} cursor="pointer">
+    Favoritos
+  </Text>,
+];
 
 export function NavBar() {
-    return (
-        <Stack bg="#EFF1F4">
-            {/* Header */}
-            <Box bg="#1DAF87" py={3} px={8} h='86px'>
-                <Flex align="center">
-                    <Link href='/' _hover={{ color: "#CFFCEE", transform: "scale(1.05)" }}
-                        _active={{ color: "#A0EACB", transform: "scale(0.95)" }}
-                        transition="all 0.2s ease">
-                        <Image src={LogoPng} h={53} w={284} cursor={'pointer'} textDecoration={'none'} />
-                    </Link>
-                    <Spacer />
-                    <HStack spaceX={20} mt={8} alignItems={'center'} textAlign={'center'}>
-                        <Link href="/history" textDecoration="none" _hover={{ color: "#CFFCEE", transform: "scale(1.05)" }}
-                            _active={{ color: "#A0EACB", transform: "scale(0.95)" }}
-                            transition="all 0.2s ease">
-                            <Text color="white" fontWeight="normal" fontSize={16} cursor="pointer">
-                                Histórico
-                            </Text>
-                        </Link>
-                        <Link href="/product" textDecoration="none" _hover={{ color: "#CFFCEE", transform: "scale(1.05)" }}
-                            _active={{ color: "#A0EACB", transform: "scale(0.95)" }}
-                            transition="all 0.2s ease">
-                            <Text color="white" fontWeight="normal" fontSize={16} cursor="pointer">
-                                Incluir Item
-                            </Text>
-                        </Link>
-                        <Link href="/favorits" textDecoration="none" _hover={{ color: "#CFFCEE", transform: "scale(1.05)" }}
-                            _active={{ color: "#A0EACB", transform: "scale(0.95)" }}
-                            transition="all 0.2s ease">
-                            <Text color="white" fontWeight="normal" fontSize={16} cursor="pointer">
-                                Favoritos
-                            </Text>
-                        </Link>
-                    </HStack>
-                    <Spacer />
-                    <Avatar/>
-                </Flex>
-            </Box>
-            <CustomBreadcrumb />
-
-        </Stack>
-    );
+  return (
+    <Stack bg="#EFF1F4">
+      {/* Header */}
+      <Box bg="#1DAF87" py={3} px={8} h="86px">
+        <Flex align="center" justifyContent={"space-between"}>
+          {childrenLinks.map((childrenLink) => (
+            <RenderLink
+              key={childrenLink!.toString()}
+              children={childrenLink}
+            />
+          ))}
+          <Avatar />
+        </Flex>
+      </Box>
+      <CustomBreadcrumb />
+    </Stack>
+  );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,21 +2,20 @@
 import { Layout } from "@/components/ui/Layout";
 import { Favoritos } from "@/features/Favoritos";
 import { HistoricoTrocas } from "@/features/HistoricoTrocas";
-import { HomeWeb } from "@/features/HomeWeb";
+// import { HomeWeb } from "@/features/HomeWeb";
 import { CreateProduct } from "@/features/product/CreateProduct/CreateProduct.page";
 import { BrowserRouter, Route, Routes } from "react-router";
 
 export function AppRouter() {
   return (
     <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<HomeWeb />} />
-          <Route path="/product" element={<CreateProduct />} />
-          <Route path="/history" element={<HistoricoTrocas />} />
-          <Route path="/favorits" element={<Favoritos />} />
-        </Routes>
-      </Layout>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="product" element={<CreateProduct />} />
+          <Route path="history" element={<HistoricoTrocas />} />
+          <Route path="favorits" element={<Favoritos />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
