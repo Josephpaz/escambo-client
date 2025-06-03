e3f1e55dd671e130c526097ee7fcc06f43ea3ccc
jspaz <joseph@suamei.com.br>
2025-06-02 20:13:32 -0400

    feat: grid by img size;

--- src/features/post/PostDetail/PostDetail.tsx
+++ src/features/post/PostDetail/PostDetail.tsx
@@ -12,14 +12,14 @@ import { useQuery } from "@tanstack/react-query";
 import { PostService } from "@/service/post/index.service";
 
 const images = [
-  "https://picsum.photos/200",
-  "https://picsum.photos/201",
-  "https://picsum.photos/202",
-  "https://picsum.photos/203",
-  "https://picsum.photos/204",
-  "https://picsum.photos/205",
-  "https://picsum.photos/206",
-  "https://picsum.photos/207",
+  "https://picsum.photos/200/120",
+  "https://picsum.photos/201/120",
+  "https://picsum.photos/202/120",
+  // "https://picsum.photos/203/120",
+  // "https://picsum.photos/204/120",
+  // "https://picsum.photos/205/120",
+  // "https://picsum.photos/206/120",
+  // "https://picsum.photos/207/120",
 ];
 
 export function PostDetail() {
@@ -198,67 +198,148 @@ export function PostDetail() {
           minW="320px"
           maxW="850px"
         >
-          <Grid templateColumns="2fr 2fr" gap={2} mb="4">
-            <Box onClick={() => handleClick(0)} cursor="pointer">
+          {images.length === 1 && (
+            <Box onClick={() => handleClick(0)} cursor="pointer" maxW="4xl" maxH="300px" h="100%">
               <Image
                 src={images[0]}
-                alt="Main preview"
-                height="100%"
+                alt="Preview 0"
+                h="100%"
                 objectFit="cover"
                 borderRadius="md"
-                maxW="100%"
+                width="100%"
               />
             </Box>
+          )}
 
-            <Grid templateColumns="1fr 1fr" gap={2} height="100%">
-              {images.slice(1, 5).map((src, idx) => {
-                const imageIndex = idx + 1;
-                const isLast = imageIndex === 4 && images.length > 5;
-                return (
-                  <Box
-                    key={imageIndex}
-                    position="relative"
-                    onClick={() => handleClick(imageIndex)}
-                    cursor="pointer"
-                  >
+          {images.length === 2 && (
+            <Grid templateColumns="repeat(2, 1fr)" gap={2} mb="4" maxH="300px">
+              {images.map((src, idx) => (
+                <Box key={idx} onClick={() => handleClick(idx)} cursor="pointer" h="100%">
+                  <Image
+                    src={src}
+                    alt={`Preview ${idx}`}
+                    h="100%"
+                    objectFit="cover"
+                    borderRadius="md"
+                    width="100%"
+                  />
+                </Box>
+              ))}
+            </Grid>
+          )}
+
+          {images.length === 3 && (
+            <Grid templateColumns="2fr 1fr" gap={2} mb="4" maxH="300px" h="100%">
+              <Box onClick={() => handleClick(0)} cursor="pointer" h="100%">
+                <Image
+                  src={images[0]}
+                  alt="Main preview"
+                  h="100%"
+                  objectFit="cover"
+                  borderRadius="md"
+                  width="100%"
+                />
+              </Box>
+              <Grid templateRows="repeat(2, 1fr)" gap={2} h="100%">
+                {images.slice(1).map((src, idx) => (
+                  <Box key={idx + 1} onClick={() => handleClick(idx + 1)} cursor="pointer">
                     <Image
                       src={src}
-                      alt={`Preview ${imageIndex}`}
-                      height="100%"
+                      alt={`Preview ${idx + 1}`}
+                      h="100%"
                       objectFit="cover"
                       borderRadius="md"
-                      maxW="100%"
+                      width="100%"
                     />
-                    {isLast && (
-                      <Flex
-                        position="absolute"
-                        top={0}
-                        left={0}
-                        right={0}
-                        bottom={0}
-                        bg="blackAlpha.600"
-                        align="center"
-                        justify="center"
-                        borderRadius="md"
-                      >
-                        <Text color="white" fontSize="xl" fontWeight="bold">
-                          +{images.length - 5}
-                        </Text>
-                      </Flex>
-                    )}
                   </Box>
-                );
-              })}
+                ))}
+              </Grid>
             </Grid>
-          </Grid>
+          )}
+
+          {images.length >= 4 && (
+            <Grid templateColumns="2fr 3fr" gap={2} mb="4" maxH="300px">
+              <Box onClick={() => handleClick(0)} cursor="pointer">
+                <Image
+                  src={images[0]}
+                  alt="Preview 0"
+                  h="272px"
+                  objectFit="cover"
+                  borderRadius="md"
+                  width="100%"
+                />
+              </Box>
+
+              <Grid
+                templateColumns="1fr 1fr"
+                templateRows="1fr 1fr"
+                gap={2}
+              >
+                <Box onClick={() => handleClick(1)} cursor="pointer">
+                  <Image
+                    src={images[1]}
+                    alt="Preview 1"
+                    h="132px"
+                    objectFit="cover"
+                    borderRadius="md"
+                    width="264px"
+                  />
+                </Box>
+                <Box onClick={() => handleClick(2)} cursor="pointer">
+                  <Image
+                    src={images[2]}
+                    alt="Preview 2"
+                    h="132px"
+                    objectFit="cover"
+                    borderRadius="md"
+                    width="264px"
+                  />
+                </Box>
+
+                {/* Última imagem (img[3]) com overlay se houver mais */}
+                <Box
+                  gridColumn="1 / span 2"
+                  position="relative"
+                  onClick={() => handleClick(3)}
+                  cursor="pointer"
+                >
+                  <Image
+                    src={images[3]}
+                    alt="Preview 3"
+                    h="132px"
+                    objectFit="cover"
+                    borderRadius="md"
+                    width="100%"
+                  />
+                  {images.length > 4 && (
+                    <Flex
+                      position="absolute"
+                      top={0}
+                      left={0}
+                      right={0}
+                      bottom={0}
+                      bg="blackAlpha.600"
+                      align="center"
+                      justify="center"
+                      borderRadius="md"
+                    >
+                      <Text color="white" fontSize="xl" fontWeight="bold">
+                        +{images.length - 4}
+                      </Text>
+                    </Flex>
+                  )}
+                </Box>
+              </Grid>
+            </Grid>
+          )}
 
           <Box>
             <Text textStyle="3xl" color="#373E4B" fontWeight="700">
               {postDetail?.titulo}
             </Text>
-            {/* <Text textStyle="2xl" color="#373E4B" mt="2" fontWeight="400">
+            <Text textStyle="2xl" color="#373E4B" mt="2" fontWeight="400">
               {postDetail?.categoria}
-            </Text> */}
+            </Text>
             <Text
               textStyle="md"
               color="#373E4B"
