b2560ea6fccbbb7871bf35476ab486241197b2a7
jspaz <joseph@suamei.com.br>
2025-06-08 16:34:34 -0400

    add upload files on create post;

--- src/service/post/index.service.ts
+++ src/service/post/index.service.ts
@@ -4,7 +4,6 @@ export namespace PostService {
   export type CreateProps = {
     titulo: string;
     descricao: string;
-    imagem_url: string;
     user_id: string;
     categoria: string;
   };
@@ -16,12 +15,25 @@ export class PostService {
     return API.post<Response>(baseUrl, payload);
   }
 
+  static uploadImage(id: string, file: File) {
+    type Response = {
+      url: string;
+    };
+    const formData = new FormData();
+    formData.append("image", file);
+    return API.post<Response>(`${baseUrl}/${id}/imagem`, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+  }
+
   static getById(id: string) {
     type Response = {
-      categoria: string
+      categoria: string;
       descricao: string;
       titulo: string;
-      user_id: string
+      user_id: string;
     };
     return API.get<Response>(`${baseUrl}/${id}/detalhes`);
   }
